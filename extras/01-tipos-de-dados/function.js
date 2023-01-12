44cd2dcb2c9ce0a5fdf2d2d1eedae2bfabf3c0b7
AdrianoBispo <Drianvini10@gmail.com>
2023-01-12 00:03:00 -0300

    pasta escopo apagada

--- extras/01-tipos-de-dados/function.js
+++ extras/01-tipos-de-dados/function.js
@@ -20,3 +20,32 @@ teste2('teste2');
 const teste3 = (parametro, p2) => console.log(parametro)
 teste3('teste3');
 */
+
+console.log(`
+## Escopo
+
+- Escopo global
+- Escopo de função
+- Escopo de bloco
+
+`);
+
+/*
+const variavelGlobal = 1;
+
+const teste = () => {
+  console.log(variavelGlobal);
+  const variavelDeFunção = 2;
+  console.log(variavelDeFunção);
+};
+teste();
+// console.log(variavelDeFunção);
+
+{
+  console.log(variavelGlobal);
+  const variavelDeBloco = 3;
+  console.log(variavelDeBloco);
+}
+console.log(variavelDeBloco);
+
+*/
